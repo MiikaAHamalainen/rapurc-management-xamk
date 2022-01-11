d4332361c23a0dd27fe1b1277889c5aa37e6cd1c
Tianxing Wu <tianxing.wu@metatavu.fi>
2022-01-11 16:19:20 +0200

    added text

--- src/components/screens/surveys-screen.tsx
+++ src/components/screens/surveys-screen.tsx
@@ -273,22 +273,36 @@ const SurveysScreen: React.FC = () => {
   /**
    * Renders delete survey dialog
    */
-  const renderDeleteSurveyDialog = () => (
-    <GenericDialog
-      error={ false }
-      open={ deletingSurvey }
-      onClose={ () => setDeletingSurvey(false) }
-      onCancel={ () => setDeletingSurvey(false) }
-      onConfirm={ onDeleteSurveyConfirm }
-      title={ strings.surveysScreen.deleteSurveysDialog.title }
-      positiveButtonText={ strings.generic.confirm }
-      cancelButtonText={ strings.generic.cancel }
-    >
-      <Typography>
-        { strings.surveysScreen.deleteSurveysDialog.text }
-      </Typography>
-    </GenericDialog>
-  );
+  const renderDeleteSurveyDialog = () => {
+    let deletingOthers = false;
+    selectedSurveyIds.every(selectedSurveyId => {
+      deletingOthers = surveysWithInfo.find(surveyWithInfo => surveyWithInfo.id === selectedSurveyId)?.creatorId !== keycloak?.profile?.id;
+      if (deletingOthers) return false;
+      return true;
+    });
+
+    return (
+      <GenericDialog
+        error={ false }
+        open={ deletingSurvey }
+        onClose={ () => setDeletingSurvey(false) }
+        onCancel={ () => setDeletingSurvey(false) }
+        onConfirm={ onDeleteSurveyConfirm }
+        title={ strings.surveysScreen.deleteSurveysDialog.title }
+        positiveButtonText={ strings.generic.confirm }
+        cancelButtonText={ strings.generic.cancel }
+      >
+        <Typography>
+          { strings.surveysScreen.deleteSurveysDialog.text }
+        </Typography>
+        { deletingOthers &&
+          <Typography color="red" fontWeight={ 600 }>
+            { strings.surveysScreen.deleteSurveysDialog.deletingOthers }
+          </Typography>
+        }
+      </GenericDialog>
+    );
+  };
 
   /**
    * Render header content
